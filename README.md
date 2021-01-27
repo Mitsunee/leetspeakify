926fb4dca3b73a49f03bb47e14fc78a67f2ad63d
Mitsunee <michelle@mitsunee.com>
2021-01-27 16:48:04 +0100

    add false value for override and new argument

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # leetspeakify
 Convert String into leetspeak or any custom alphabet
 
+Note: This is mostly a joke project. I'm using it as an example project to learn about publishing on npm.
+
 # Install
 
 `npm install @mitsunee/leetspeakify`
+
+# Overview
+
+```
+string leetspeakify(string subject [, object override [, boolean useDefault]])
+```
+
+**Parameters**:
+
+- `subject` (String): The string to leetspeakify
+- `override` (Object, optional): Custom alphabet. Non-object values as well as Arrays are ignored.
+- `useDefault` (Boolean, optional): *Default: `true`* Can be set to `false` to disable the default alphabet.
 
 # Usage
 
@@ -12,8 +26,22 @@ Convert String into leetspeak or any custom alphabet
 const leetspeakify = require("@mitsunee/leetspeakify");
 
 // Convert to leetspeak
-leetspeakify("Hello!"); // '|-|3|_|_0 \\/\\/0|2|_|)!'
+leetspeakify("Hello World!"); // |-|3|_|_0 \/\/0|2|_|)!
 
 // Use custom alphabet override
-leetspeakify("Hello World!", {e: "€"}) // '|-|€|_|_0 \\/\\/0|2|_|)!'
+leetspeakify("Hello World!", {e: "€"}) // |-|€|_|_0 \/\/0|2|_|)!
+
+// Disable default alphabet per-character
+leetspeakify("Hello World!", {e: false}) // |-|e|_|_0 \/\/0|2|_|)!
+
+// Disable default alphabet completely
+leetspeakify("Hello World!", {e: "€"}, false) // H€llo World!
 ```
+
+# Changelog
+
+- 1.0.3
+    - Added `useDefault` argument to disable default alphabet with `false`
+    - Added `false` as a possible value for 2nd argument, which disables that character in the default alphabet
+- 1.0.2 & 1.0.1
+    - Just me messing up my own commit history and figuring out `npm publish` (:
--- src/leetspeakify.js
+++ src/leetspeakify.js
@@ -1,19 +1,30 @@
-module.exports = function (subject, override) {
+module.exports = function (subject, override, useDefault) {
+    // reject non-string argument
     if (typeof subject !== "string") return false;
-    let hasOverride = false;
+
+    // set defaults
+    let hasOverride = false,
+        hasDefault = true;
+
+    // if override exists and is non-array object enable override
     if (typeof override === "object" && !(override instanceof Array))
         hasOverride = true;
 
+    // if useDefault argument is true disable default alphabet
+    if (useDefault === false) hasDefault = false;
+
+    // default alphabet
     let alphabet = {
         a: "4", b: "ß", c: "(", d: "|)", e: "3", f: "|=", g: "6", h: "|-|", i: "|", j: "_|",
         k: "|<", l: "|_", m: "|\\/|", n: "|\\|", o: "0", p: "|°", q: "0,", r: "|2", s: "$",
         t: "7", u: "|_|", v: "\\/", w: "\\/\\/", x: "><", y: "`/", z: "2"
     }
 
-    return [...subject].map(char => {
-        let c = String(char).toLowerCase();
-        if (hasOverride && override[c]) return override[c];
-        if (alphabet[c]) return alphabet[c];
-        return char;
-    }).join("");
+    return [...subject].map(char => { // split subject into array and then map
+        let c = String(char).toLowerCase(); // normalize to lowercase
+        if (hasOverride && override[c]) return override[c]; // return override if set
+        if (hasOverride && override[c] === false) return char; // return char if override is false
+        if (hasDefault && alphabet[c]) return alphabet[c]; // return default alphabet if not disabled
+        return char; // if neither of the above applied return char
+    }).join(""); // re-join the mapped subject array
 }
